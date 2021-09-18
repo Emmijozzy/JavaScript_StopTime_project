22c19fbe7043910ff4029c72b1dde38404fd7b7c
Emmijozzy <josephogunsuyi360@gmail.com>
2021-09-18 16:23:09 +0100

    hjakkaa

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Definition of Element
   let hr = document.querySelector(".hr");
   let min = document.querySelector(".min");
   let sec = document.querySelector(".sec");
@@ -6,12 +7,19 @@
   let stop_btn = document.querySelector(".stop-btn");
   let reset_btn= document.querySelector(".reset-btn");
 
+//Construction function
  let Timee = function(){
+   //definition of properties
   this.jsMilisec =0;
   this.jsHr = 0;
   this.jsSec = 0;
   this.jsMin = 0;
+  
+  //definition of methods
+        // private methods
     var init = ()=> {this.jsMilisec += 10;};
+        //publicbmethods
+    //definition of counter meth0d
   this.counter1 = () => {
     init();
     if(this.jsMilisec > 999){
@@ -31,6 +39,7 @@
      sec.innerHTML = this.jsSec < 10 ? "0" + this.jsSec : this.jsSec;
      milisec.innerHTML= this.jsMilisec < 10 ? "00" + this.jsMilisec : this.jsMilisec < 100 ? "0" + this.jsMilisec : this.jsMilisec;
   }
+    // definition of reset method
     this.reset = () => {
      this.jsHr = 0;
       this.jsSec = 0;
@@ -43,29 +52,23 @@
   };
 };
 
+//instantiation of Object
 let timee = new Timee();
 var timer;
-star_btn.addEventListener('click', () => {
+
+// start-btn
+star_btn.addEventListener('click', (e) => {
   timer = window.setInterval(timee.counter1, 10);
 });
 
+// stop-btn
 stop_btn.addEventListener('click', (e) => {
   clearInterval(timer);
 });
 
-
-let reseting = () =>{
+// reset-btn
+reset_btn.addEventListener('click', (e) => {
   clearInterval(timer);
   timee.reset()
-};
-reset_btn.addEventListener('click', reseting);
+});
 
-console.log(timee.counter1());
-//   let y = 1;
-// let a = () => {
-//   y++;
-//   console.log(y);
-// }
-  
-// window.setInterval(a
-// , 10)
